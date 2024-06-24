9ba7c94a5c40ef34f460fa14f683b680701c816b
Your Name <bolomycke65@gmail.com>
2024-06-24 14:10:34 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints output
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	int o;
+
+	o = 48;
+
+	while (o <= 57)
+		putchar(o);
+	if (o != 57)
+		putchar(',');
+	putchar(' ');
+	o++;
+	putchar('\n');
+	return (0);
+}
