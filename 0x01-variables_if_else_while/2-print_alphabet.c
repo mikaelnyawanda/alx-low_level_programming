a410f03cee4a2ee7f9234f78f51fb98b1880fa0c
Your Name <bolomycke65@gmail.com>
2024-06-24 13:13:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	char m;
+
+	for(m = 'a'; m <= 'z'; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
