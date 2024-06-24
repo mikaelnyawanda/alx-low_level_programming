bf5fe87d626693529ed9a246d0687e479a7bb4e2
Your Name <bolomycke65@gmail.com>
2024-06-24 14:28:15 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+	char m;
+
+	for (m = '0'  m <= '9'; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
