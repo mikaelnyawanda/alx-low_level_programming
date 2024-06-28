e52dea4e6f4e9656c0b56c64898053b388ec9202
Your Name <bolomycke65@gmail.com>
2024-06-28 13:56:22 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,20 +10,14 @@ void print_most_numbers(void)
 
 {
 
-	int x = 0;
+	int x;
 
-	for (; x <= 9; x++)
+	for (x = 0; x <= 9; x++)
 	{
-	for (if x <= 2 || x == 4)
-	{
-	continue;
-	}
-	else
+	if (x != 2 && x != 4)
 	{
 	_putchar(x + '0');
 	}
 	}
 	_putchar('\n');
 }
-
-
