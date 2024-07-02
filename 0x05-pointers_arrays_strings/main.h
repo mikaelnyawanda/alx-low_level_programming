e354f1db40ab3ebe5e8395af02f0bc3ab336d1bc
Your Name <bolomycke65@gmail.com>
2024-07-02 08:29:11 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
-@@ -0,0 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h> 
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,4 +14,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif /* MAIN_H */
+#endif
