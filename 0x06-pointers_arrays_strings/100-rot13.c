558e075c9563e45f106c12a12b13a0e841339406
Your Name <bolomycke65@gmail.com>
2024-07-03 23:31:46 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to string params
+ * rot13 - Encodes a string using rot13
+ * @s: The string to encode
  *
- * Return: *s
+ * Return: The encoded string
  */
-
 char *rot13(char *s)
 {
-	int i;
-	int j;
-	char detal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char detarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int i, j;
+char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for(j = 0; j < 52; j++)
-		{
-			if (s[i] == datal[j])
-			{
-				s[i] = datarot[j];
-				break;
-			}
-		}
-	}
-	return (s);
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+break;
+}
+}
+}
+
+return (s);
 }
