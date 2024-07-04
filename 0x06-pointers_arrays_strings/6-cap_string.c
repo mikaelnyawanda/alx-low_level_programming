42a6f45988237bf41da376b6953ab88304e8daa0
Your Name <bolomycke65@gmail.com>
2024-07-03 23:21:20 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,22 @@
 #include "main.h"
-
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
- */
+* cap_string - Capitalizes all words of a string
+* @str: The string to modify
+* Return: A pointer to the modified string
+*/
 char *cap_string(char *str)
 {
-	int index = 0;
-	while (str[index])
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-	if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||	
-		str[index - 1] == '?' ||
-		str[index - 1] == '"' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
-		index == 0)
-		str[index] -= 32;
-	index++;
+int i, j;
+char separators[] = " \t\n,;.!?\"(){}";
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+for (j = 0; separators[j] != '\0'; j++)
+{
+if (separators[j] == str[i] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+str[i + 1] -= 32;
+}
 }
 return (str);
 }
-
